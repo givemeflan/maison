391247b1217140af75c06e55ba3328e6de62af45
Laura Le <lle387@gmail.com>
2020-12-08 21:55:16 -0500

    login form added to login page, added files to configuration folder, adding texture to cart page

---
+++ maison_site/src/main/java/com/tts/maison_site/configuration/WebMvcConfiguration.java
@@ -0,0 +1,19 @@
+package com.tts.maison_site.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+}
+
+// Help us set up our password encoder
