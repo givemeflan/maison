75c2cca8e6c16fd2c4a2825ec50a911a4657242c
Laura Le <lle387@gmail.com>
2020-12-09 20:40:07 -0500

    adding cart pages

---
+++ maison_site/src/main/java/com/tts/maison_site/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.tts.maison_site.repository;
+
+import com.tts.maison_site.model.Cart;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CartRepository extends CrudRepository<Cart, Long> {
+
+}
